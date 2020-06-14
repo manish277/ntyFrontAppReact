ea7005c779214701311f4b27505b8dbdc7b2023e
Manish <manishkumaryadav189@gmail.com>
2020-06-14 22:58:23 +0530

    Updates

---
+++ precache-manifest.545a8961ee1ea84844729215ac75dd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0830ee1565125f61621db7cd1ab4e8",
+    "url": "/ntyFrontAppReact/index.html"
+  },
+  {
+    "revision": "fefcee4226e011822596",
+    "url": "/ntyFrontAppReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f151e36efdae4b1bc65f",
+    "url": "/ntyFrontAppReact/static/js/2.e1510de9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ntyFrontAppReact/static/js/2.e1510de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fefcee4226e011822596",
+    "url": "/ntyFrontAppReact/static/js/main.59a6efcf.chunk.js"
+  },
+  {
+    "revision": "c02e8cd57c64b01e622c",
+    "url": "/ntyFrontAppReact/static/js/runtime-main.f0d42c12.js"
+  }
+]);
