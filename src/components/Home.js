65ad22285b820b8d9b362bc214876b61b92377ad
Manish <manishkumaryadav189@gmail.com>
2020-06-25 18:13:30 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,15 @@ export class Home extends Component {
         return (
             <div className='mt-5 d-flex justify-content-left'>
             <h3>
-            Welcome to Nitya software.
-            This is CRUD apllication assignment for software Developent.
+            Welcome to React software.
+            <P>
+            This is CRUD apllication work for React Developent.
+            </P>
             Back End is created on Node js
             Database is MongoDB
+            <br>
             back end is diployed on HEROKU
+            </br>
             </h3>
             </div>
         )
